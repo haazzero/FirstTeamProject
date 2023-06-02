352a99479ea0836263f832a3f2069fcbc4ad8c04
SustarJin <132961202+SustarJin@users.noreply.github.com>
2023-06-02 11:10:55 +0900

    Create code.java

---
+++ 230602/code.java
@@ -0,0 +1,44 @@
+package practice230602;
+
+public class practice1 {
+
+	public static void main(String[] args) {
+			System.out.println("=========메인========");
+			System.out.println("   1. 로그인💡");
+			System.out.println("   2. 시스템 종료");
+		    System.out.println("   >> 선택 : ");
+
+
+
+
+			System.out.println("1. 로그아웃");
+			System.out.println("2. 비밀번호 변경");
+			System.out.println("3. 관리자 메뉴");
+		    System.out.println("   >> 선택 : ");
+
+
+
+
+
+			System.out.println("1. 직원관리");
+			System.out.println("2. 근태관리");
+			System.out.println("3. 급여조회");
+			System.out.println("4. 인사고과관리");
+			System.out.println("5. 시스템 종료");
+		    System.out.println("   >> 선택 : ");
+
+	
+			
+		    
+		    System.out.println("1. 직원 정보 조회");
+			System.out.println("2. 근태 조회");
+			System.out.println("3. 급여 조회");
+			System.out.println("4. 인사고과 조회");
+			System.out.println("5. 시스템 종료");
+			System.out.println("   >> 선택 : ");
+			
+			
+			
+	}
+
+}
