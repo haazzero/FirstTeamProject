221a6294650c7f8d119807873dce2f22f7dfeba8
wow_dev <119738419+wowssun@users.noreply.github.com>
2023-06-07 17:00:29 +0900

    Create AuthVO.java

---
+++ VO/AuthVO.java
@@ -0,0 +1,20 @@
+package vo;
+
+public class AuthVO {
+	private String id;  // 로그인 아이디
+	private String pw;  // 로그인 비밀번호
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+
+	
+}
